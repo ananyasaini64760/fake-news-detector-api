f6792f29ee173929cf19d5d1f82ef8b41832e5b9
Ananya Saini <ananyasaini050@gmail.com>
2025-07-14 01:15:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+import numpy as np
+import joblib
 from fastapi import FastAPI
 from pydantic import BaseModel
-import tensorflow as tf
-import joblib
+from tensorflow.lite.python.interpreter import Interpreter
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-model = tf.keras.models.load_model("fakenews_lstm_model.h5")
+app = FastAPI()
+
+# Load tokenizer
 tokenizer = joblib.load("tokenizer.pkl")
 maxlen = 40
 
-app = FastAPI()
+# Load TFLite model
+interpreter = Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
 
 class NewsRequest(BaseModel):
     text: str
@@ -17,7 +25,12 @@ class NewsRequest(BaseModel):
 def predict(news: NewsRequest):
     seq = tokenizer.texts_to_sequences([news.text])
     padded = pad_sequences(seq, maxlen=maxlen, padding='post')
-    pred = model.predict(padded)[0][0]
+
+    # Run model
+    interpreter.set_tensor(input_index, np.array(padded, dtype=np.float32))
+    interpreter.invoke()
+    pred = interpreter.get_tensor(output_index)[0][0]
+
     label = "Real" if pred >= 0.5 else "Fake"
     confidence = round(float(pred if label == "Real" else 1 - pred), 2)
     return {"label": label, "confidence": confidence}
